b3ee0cc3aef487bd749812119a3a036e695ba4ad
Goleta12 <giogoletiani1999@gmail.com>
2023-06-01 20:37:19 +0400

    axali gakveteili

---
+++ day3/gakvetili3.py
@@ -0,0 +1,86 @@
+# my_name = "giorgi"
+
+# if "g" in my_name:
+#     print("sheicavs k-s")
+# my_name = "gio"
+# my_surname = "goletiani"
+# my_pet_name = "miki"
+
+
+
+# my_sentence = "aa {2} bb {0} cc {1}".format(my_name, my_pet_name, my_surname)
+
+
+# print(my_sentence)
+
+# name1 = "gio"
+# name2 = "goletiani"
+# name3 = "23"
+
+# name4 = "me var {} chemi gvaria {} chemi asakia {}".format(name1, name2, name3)
+
+# print(name4)
+
+# my_name = "giorgi"
+
+# if "i" in my_name:
+#     print("nayiini nayini")
+# else:
+#     print("ar sheicavs")
+
+
+#input
+#output
+
+# my_age = 22
+
+# my_age += 3
+
+# name1 = input("enter your name: ")
+# name2 = input("enter your usarname: ")
+# name3 = input("enter your age: ")
+
+# print("my name is {} my usarname is {} my age is {}".format(name1, name2, name3))
+
+# new_age = int(name3) + 3
+
+# name3 = "after 3 years i am now {} years old".format(new_age)
+
+
+# print(name3)
+
+# nam1 = int(input("enter your answer: "))
+# nam2 = int(input("enter your answer2: "))
+
+# product = nam1 * nam2
+# if product > 100:
+#     print(product)
+# else:
+#     print("you loose")
+
+#print(20 % 6)       # % aris nashtis chveneba 3*6= 18 20-18=2
+
+# nika = "gio"
+# nika += "123"      # += nishnavs inkrementacia anu damateba cvladis 
+
+# print(nika)    
+
+# input, %, if, +=  gamoyenebit gavaketot amocana 
+    
+
+                            # even - luwi  odd - kenti
+
+
+nam1 = int(input("take your bet: "))
+nam2 = int(input("take your bet2: "))
+nam3 = int(input("take your bet3: "))
+
+nam1 += (35 % 4)
+nam2 += (25 % 3)
+nam3 += (13 % 2)
+
+product = (nam1, nam2, nam3)
+
+if product is even:
+    print(even)
+print(product)
